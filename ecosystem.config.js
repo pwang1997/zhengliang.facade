92b81c71a404f1256dd84b334fe539419ff0e054
zwang1997 <zwang684@uottawa.ca>
2024-03-29 16:10:42 -0400

    fix typo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,14 +7,14 @@ module.exports = {
     production : {
       key : "facade-rsa.pem",
       user : 'ubuntu',
-      host : '18.116.52.115',
+      host : '13.58.36.105',
       ref  : 'origin/main',
       repo : 'git@github.com:pwang1997/facade-client-next.git',
       path : '/home/ubuntu',
       'pre-deploy-local': '',
       'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
-      'ssh_options' : 'FowardAgent=yes'
+      'ssh_options' : 'ForwardAgent=yes'
     }
   }
 };
