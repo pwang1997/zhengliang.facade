0fa13e256fda93962253c6cf3f1763d39f51229f
Zhengliang Wang <zwang684@uottawa.ca>
2024-04-05 19:57:36 -0400

    update url

--- src/utils/restUtils.ts
+++ src/utils/restUtils.ts
@@ -1,6 +1,6 @@
-export const BACKEND_URL = 'http://localhost:8080';
+const BACKEND_URL = 'http://18.219.120.247:8080/api';
 
-export const BLOG_ENDPOINT = `${BACKEND_URL}/api/v1/posts`;
-export const PROJECT_ENDPOINT = `${BACKEND_URL}/api/v1/projects`;
-export const METRICS_ENDPOINT = `${BACKEND_URL}/api/v1/metrics`;
-export const TAG_ENDPOINT = `${BACKEND_URL}/api/v1/tags`;
+export const BLOG_ENDPOINT = `${BACKEND_URL}/v1/posts`;
+export const PROJECT_ENDPOINT = `${BACKEND_URL}/v1/projects`;
+export const METRICS_ENDPOINT = `${BACKEND_URL}/v1/metrics`;
+export const TAG_ENDPOINT = `${BACKEND_URL}/v1/tags`;
