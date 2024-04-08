9f9846e6475ff3bcbfc4e89e019206ee60899086
Zhengliang Wang <zwang684@uottawa.ca>
2024-04-08 16:47:31 -0400

    update: added site note

--- src/app/site-notes/site-notes.ts
+++ src/app/site-notes/site-notes.ts
@@ -9,8 +9,13 @@ const _2024_03_26 = {
     text: "Included dark/light mode for the webisite; Added side panel for the system entity overview."
 }
 
+const _2024_04_04 = {
+    date : "April 4th, 2024",
+    text : `Registered the project to domain http://blog.pwang.facademe.xyz, and later the night got DDos 🤔. 
+    While I am still learning stuff about cyber security, I decided to temporarily pull the deployment donw until I am done adding Spring Cloud Gateway + Sentinel for network flow control test.`
+}
 
 
-const notes = [_2024_03_15, _2024_03_26];
+const notes = [_2024_04_04, _2024_03_26, _2024_03_15, ];
 
 export default notes;
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,7 +4,6 @@ import { Popover } from '@headlessui/react';
 import DarkModeSwitcher from 'components/button/DarkModeSwitcher';
 import GitHubIcon from 'icons/GitHubIcon';
 import Link from 'next/link';
-import { aboutConfig } from './config';
 import NavBarItem from './NavBarItem';
 
 export default function NavBar() {
@@ -22,7 +21,8 @@ export default function NavBar() {
         <Popover.Group className='hidden lg:flex lg:gap-x-12 items-center'>
           <NavBarItem name='Projects' href='/projects' />
           <NavBarItem name='Blogs' href='/blogs' />
-          <NavBarItem name='About' subMenuItems={aboutConfig} />
+          <NavBarItem name='About' href='about' />
+          <NavBarItem name='Site Notes' href='/site-notes' />
           <NavBarItem href='https://github.com/pwang1997'>
             <GitHubIcon />
           </NavBarItem>
