9c65bcfa61a42c5007c3686a148e9452f019f14b
Zhengliang Wang <zwang684@uottawa.ca>
2024-04-11 11:37:00 -0400

    feat: restyle projects page with partitioned cards

--- src/app/projects/components/ProjectCard.tsx
+++ src/app/projects/components/ProjectCard.tsx
@@ -44,27 +44,14 @@ export default function ProjectCard({ project, timeout }: { project: Project, ti
         dark:border-slate-300/75 border-1 animated`}>
             <div>
               <div className="font-bold text-xl my-2 line-clamp-1 dark:bg-medium dark:text-white">
-                <Link href={`/projects/${project.id}`} className="underline">
+                <Link href={`github.com/${project?.sourceCodeUrl}`} className="underline">
                   {project.title}
                 </Link>
               </div>
               <p className="text-gray-900 text-base line-clamp-3 dark:bg-medium dark:text-white">{project.summary}</p>
             </div>
-            <div className="container flex flex-col gap-2  link-set">
-              {!!project.sourceCodeUrl && sourceCodeUrl(`github.com/${project.sourceCodeUrl}`)}
-              {!!project.demoUrl && demoUrl(project.demoUrl)}
-            </div>
-
             <div>
               <TagChipList tags={project.tags ?? []} />
-            </div>
-            <div className="flex justify-end float-right">
-              <Link
-                className="bg-grey-900  text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline dark:bg-medium dark:text-white dark:hover:bg-slate-500"
-                href={`/projects/${project.id}`}
-              >
-                Learn More
-              </Link>
             </div>
           </div>
         )
--- src/app/projects/components/ProjectsContent.tsx
+++ src/app/projects/components/ProjectsContent.tsx
@@ -4,6 +4,7 @@ import CategoryPanel, { CategoryPanelDataProp } from "components/category-panel"
 import { Tag } from "models/post";
 import { Project } from "models/project";
 import { useCallback, useMemo, useState } from "react";
+import { partitionArray } from "utils/misc-utils";
 import ProjectCard from "./ProjectCard";
 
 export default function ProjectsContent({ projects, projectTagUsage }: { projects: Project[], projectTagUsage: CategoryPanelDataProp[] }) {
@@ -26,17 +27,27 @@ export default function ProjectsContent({ projects, projectTagUsage }: { project
 
         const selectedProjectIds = (!!selectedTag ? filteredSimpleProjects : simpleProjects).map((item) => item.id);
 
-        const filteredProjects = projects.filter((project) => selectedProjectIds.includes(project.id))
+        const filteredProjects = projects.filter((project) => selectedProjectIds.includes(project.id));
+
+        const partitionedProjects = partitionArray(filteredProjects, filteredProjects.length >= 3 ? 3 : 1);
         return (
-            <>
-                {filteredProjects?.map((project: Project, idx : number) => {
-                    return (
-                        <div className="project-card-container" key={project.id}>
-                            <ProjectCard project={project} timeout={400 * idx} />
-                        </div>
-                    );
-                })}
-            </>
+            <div className="flex flex-row gap-2">
+                {
+                    partitionedProjects.map((projects_: Project[], idx : number) => {
+                        return (
+                            <div className="flex flex-col gap-4" key={idx}>
+                                {projects_?.map((project: Project, idx: number) => {
+                                    return (
+                                        <div className="project-card-container" key={project.id}>
+                                            <ProjectCard project={project} timeout={400 * idx} />
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        )
+                    })
+                }
+            </div>
         )
     }, [projects, selectedTag, simpleProjects])
 
