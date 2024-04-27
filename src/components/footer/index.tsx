bd98fde71e164d8aca34cc0fc6369ee0d50b1b76
Zhengliang Wang <zwang684@uottawa.ca>
2024-04-27 17:47:26 -0400

    feat: add vercel analytics

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import GitHubIcon from "icons/GitHubIcon";
 import Link from "next/link";
 
@@ -27,6 +29,8 @@ export default function Footer() {
                     </li>
                 </ul>
             </div>
+            <Analytics />
+            <SpeedInsights />
         </footer>
 
     )
