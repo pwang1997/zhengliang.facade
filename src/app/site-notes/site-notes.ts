23e1cf49f20c34b8914342c8f03db5320a1e342c
Zhengliang Wang <zwang684@uottawa.ca>
2024-04-27 17:41:33 -0400

    update resume

--- src/app/site-notes/site-notes.ts
+++ src/app/site-notes/site-notes.ts
@@ -18,6 +18,10 @@ const _2024_04_12 = {
     text : "Included GitHub comment section for blog posts."
 }
 
-const notes = [_2024_04_12, _2024_04_04, _2024_03_26, _2024_03_15, ];
+const _2024_04_27 = {
+    date : "April 27th, 2024",
+    text : "Great! My blog.v1 is offically launched!🤞"
+}
+const notes = [_2024_04_12, _2024_04_04, _2024_03_26, _2024_03_15, _2024_04_27];
 
 export default notes;
