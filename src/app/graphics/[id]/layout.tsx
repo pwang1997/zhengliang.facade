31ce302b8712dad032aaa32cad8074bf3563eeae
Zhengliang Wang <puck19970418@gmail.com>
2024-06-03 12:01:08 -0400

    restore footer style

--- src/app/graphics/[id]/layout.tsx
+++ src/app/graphics/[id]/layout.tsx
@@ -4,5 +4,5 @@ export default function RootLayout({
   }: Readonly<{
     children: React.ReactNode;
   }>) {
-    return <div className="lg:container lg:mx-auto flex flex-col gap-2 p-4 m-4 w-full max-w-2xl dark:bg-medium dark:text-white">{children}</div>;
+    return <div className="lg:container h-[calc(100vh-64px)] lg:mx-auto flex flex-col gap-2 p-4 m-4 w-full max-w-2xl dark:bg-medium dark:text-white">{children}</div>;
   }
